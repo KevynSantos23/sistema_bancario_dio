ed7068946b4d871d6214f9e9230229fab6996387
Kevyn Santos <kevynsant.ss@gmail.com>
2024-04-14 17:34:37 -0300

    Criação de metodo para reconher sistema e limpar terminal

---
+++ sistema.py
@@ -0,0 +1,23 @@
+import os
+import platform
+
+class Sistema():
+
+    sistema = platform.system()
+
+    def limpar_terminal(sistema):
+
+        match sistema:
+            case "Windows":
+                # "Estamos executando no Windows."
+                limpar_terminal = os.system('cls')
+            case "Linux":
+                # "Estamos executando no Linux."
+                limpar_terminal = os.system('clear')
+            case "Darwin":
+                # "Estamos executando no macOS."
+                limpar_terminal = os.system('clear')
+            case _:
+                # "Sistema operacional não reconhecido"
+                print("Sistema operacional não reconhecido")
+                limpar_terminal = os.system('clear')
