7bfebe212c0da6d2c365013e61482ca880fee355
Kevyn Santos <kevynsant.ss@gmail.com>
2024-04-14 22:21:03 -0300

    Doc Update 'app.py': Atualização no arquivo

--- app.py
+++ app.py
@@ -15,37 +15,39 @@ menu = """
 
 conta = {
     'saldo': 0.0,
-    'limite': 500.0,
-    'extrato': "",
+    'LIMITE_SAQUE': 500.0,
+    'extrato': [],
     'numero_saques': 0,
     'LIMITE_SAQUES': 3
 }
+continuar_operando = True
 
-while True:
+while continuar_operando:
 
     escolha_operacao = input(menu).upper()
+    operacoes = Operacoes(escolha_operacao, conta)
 
     match escolha_operacao:
-
+            
         case "D":
             sistema.limpar_terminal()
-            print("Depósito")
-            operacao_deposito = Operacoes(escolha_operacao, conta)
-            conta["saldo"] = operacao_deposito.dados_conta["saldo"]
-
-            continuar_operando = input("""\nRealizar outra operação? [s]- Sim | [n] - Não\n""").upper()
+            print("___Depósito___")
+            operacoes.operacao_deposito()
+            conta["saldo"] = operacoes.dados_conta["saldo"]
             
-            match continuar_operando:
-                case "S":
-                    sistema.limpar_terminal()
-                    continue
-                case "N":
-                    sistema.limpar_terminal()
-                    break
+
+            continuar_operando = operacoes.continuar_operando()
+            
+            
             
         case "S":
-            print("Saque")
-            print()
+            sistema.limpar_terminal()
+            print("___Saque___")
+            operacoes.operacao_sacar()
+            conta["saldo"] = operacoes.dados_conta["saldo"]
+
+            continuar_operando = operacoes.continuar_operando()
+
         case "E":
             print("Extrato")
             print()
